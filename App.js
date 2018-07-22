2ea940f8930a8697c9a433ad8234bd88d3f52ee0
Robson Carvalho <robsonpca@gmail.com>
2018-07-22 12:01:55 -0300

    Included support to hardware backbutton press.

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { BackHandler } from 'react-native';
-import { NavigationActions } from 'react-navigation';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 
 import AppReducer from './src/reducers';
 import { AppNavigator, middleware } from './src/navigators/AppNavigator';
@@ -14,26 +10,7 @@ const store = createStore(AppReducer, applyMiddleware(middleware));
 
 class ReduxExampleApp extends React.Component {
   componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
   }
-
-  componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
-  }
-
-  onBackPress = () => {
-    console.log('back button clicked');
-    console.log(this.props);
-    const { dispatch, nav } = this.props;
-    /* if (nav.index === 0) {
-      return false;
-    } */
-
-    // this.props.navigation.dispatch(NavigationActions.back());
-    return true;
-  };
-
-
   render() {
     return (
       <Provider store={store}>
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import lifecycle from 'react-pure-lifecycle';
+// import { NavigationActions } from 'react-navigation';
+import { StyleSheet, View, BackHandler } from 'react-native';
 
 import LoginStatusMessage from './LoginStatusMessage';
 import AuthButton from './AuthButton';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -12,6 +15,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
 });
+
+let globalprop;
+
+const onBackPress = () => {
+  console.log('back button clicked');
+
+  // globalprop.navigation.dispatch({ type: 'Login' });
+  // globalprop.navigation.dispatch(NavigationActions.back());
+  globalprop.navigation.pop();
+  return true;
+};
+
+const lifecycleMethods = {
+  componentDidMount(props) {
+    globalprop = props;
+    BackHandler.addEventListener('hardwareBackPress', onBackPress);
+  },
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+  },
+};
+
 
 const MainScreen = () => (
   <View style={styles.container}>
@@ -24,4 +50,4 @@ MainScreen.navigationOptions = {
   title: 'Home Screen',
 };
 
-export default MainScreen;
+export default lifecycle(lifecycleMethods)(MainScreen);
